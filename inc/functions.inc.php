98ae8ca398eafc59b1d2b3954522c73443f6d442
haoyixin <haoyixin@live.cn>
2013-08-15 03:00:41 +0800

    sendEmail works and added auth

--- inc/functions.inc.php
+++ inc/functions.inc.php
@@ -10,22 +10,23 @@ Generic functions theat may be called from many differant places
 
 function sendEmail($settings, $subject, $body) {
 
+	if ($settings['alertEnable'] == true){
 		$mailSettings = array(
-				'host' => $settings['alertSmtp']
+				'host' => $settings['alertSmtp'],
 			
-				/* ,
+				
 				'auth' => true,
-				'username' => $username,
-				'password' => $password,
+				'username' => $settings['alertUser'],
+				'password' => $settings['alertPass'],
 				'port' => '25'
-				*/
+				
 			);
 	
 		//$settings['alertDevice']
 
 		$mail = Mail::factory("smtp", $mailSettings );
 
-		$headers = array("From"=>$settings['alertEmail'], "Subject"=>$subject);
+		$headers = array("From"=>$settings['alertUser'], "Subject"=>$subject);
 		$mail->send($settings['alertEmail'], $headers, $body);		
-
+	}
 }
